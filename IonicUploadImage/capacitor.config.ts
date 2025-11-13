1e2a36b57cf68376060561446e766a287633ddc8
Kiana Ramzanzadeh <138225536+KianaRm@users.noreply.github.com>
2025-11-14 01:14:24 +0300

    Delete IonicUploadImage directory

--- IonicUploadImage/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: ' com.kianaramzanzadeh.ionicupload',
-  appName: 'ionicupload',
-  webDir: 'www'
-};
-
-export default config;
