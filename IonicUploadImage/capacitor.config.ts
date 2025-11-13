4b98a4ce5544e27d2942acee9c783bf0b1947151
Kiana Ramzanzadeh <138225536+KianaRm@users.noreply.github.com>
2025-11-13 11:47:02 +0300

    Add files via upload

---
+++ IonicUploadImage/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: ' com.kianaramzanzadeh.ionicupload',
+  appName: 'ionicupload',
+  webDir: 'www'
+};
+
+export default config;
